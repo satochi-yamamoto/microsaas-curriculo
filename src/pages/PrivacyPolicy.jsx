f01278d3ab8cc3d9a1a866c4307d0d408a7c2d5f
ALEXANDRE SATOCHI YAMAMOTO <108668607+satochi-yamamoto@users.noreply.github.com>
2025-07-05 11:59:47 -0300

    Add AdSense meta tag to pages

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -10,6 +10,7 @@ function PrivacyPolicy() {
       <Helmet>
         <title>Política de Privacidade</title>
         <meta name="description" content="Saiba como tratamos seus dados no gerador de currículos." />
+        <meta name="google-adsense-account" content="ca-pub-4789090074866563" />
       </Helmet>
       <h1 className="text-3xl font-bold mb-4 text-white">Política de Privacidade</h1>
       <p className="mb-4 text-gray-300">
